6ccf8cc2e96d17a6b7d7b99dfdd30845bdd85ec5
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-05-30 04:12:26 +0300

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    
+    def __init__(self, value = ""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if type(value) == str:
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        else:
+            return False
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        else:
+            return False
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        else:
+            return False
